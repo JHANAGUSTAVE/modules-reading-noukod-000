aaf867c9365bcc49a5e5624e53215f6b279f32d0
Jhana Gustave <jhana.gustave@gmail.com>
2019-05-09 19:24:52 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -1,15 +1,4 @@
-module FancyDance
-  module InstanceMethods
-    def superjump 
-      "I'm just one jumping instance"
-    end
-  end
-    module ClassMethods
-    def megajump 
-      "I'm a whole class of MOFO's jumping"
-    end
-  end 
-end
+
 module MetaDancing
   def metadata
     "This class produces objects that love to dance."
