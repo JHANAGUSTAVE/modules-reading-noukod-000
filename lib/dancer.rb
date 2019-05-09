15d843d61df02d777c57cb81c01ece096d9d99f6
Jhana Gustave <jhana.gustave@gmail.com>
2019-05-09 19:42:58 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,4 +4,10 @@ require_relative './dance_module.rb'
 class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+  include Dance       
+  extend MetaDancing  
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
 end
