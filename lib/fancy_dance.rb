7b7d197738f0874b68f76883ae716c81b24e1c7a
Jhana Gustave <jhana.gustave@gmail.com>
2019-05-09 19:43:48 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -28,3 +28,17 @@ module FancyDance
     
   end
 end
+
+
+module FancyDance
+  module InstanceMethods              # you "include" these instance methods
+    def superjump 
+      "I'm just one jumping instance"
+    end
+  end
+  module ClassMethods                 # you "extend" these class methods
+    def megajump 
+      "I'm a whole class of MOFO's jumping"
+    end
+  end 
+end
