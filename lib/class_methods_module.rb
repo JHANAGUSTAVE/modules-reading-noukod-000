af2c383c2802699fc39176f3f0fc2b40498a2258
Jhana Gustave <jhana.gustave@gmail.com>
2019-05-09 19:23:42 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -1,4 +1,9 @@
-
+module FancyDance
+  module InstanceMethods
+    def superjump 
+      "I'm just one jumping instance"
+    end
+  end
     module ClassMethods
     def megajump 
       "I'm a whole class of MOFO's jumping"
